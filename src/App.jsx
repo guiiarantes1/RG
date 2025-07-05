b5e195a104c3513439fe781c6775e84f7cabc59c
Guilherme Arantes <guii_arantes@hotmail.com>
2025-07-05 10:26:16 -0300

    feature/colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Login from './pages/Login'
+import { useTheme } from './hooks/useTheme'
 import './App.css'
 
 function App() {
+  const { theme } = useTheme();
+
   return (
-    <div className="App">
+    <div className="App" data-theme={theme}>
       <Login />
     </div>
   )
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -85,7 +85,7 @@ const Login = () => {
                 onClick={() => setShowPassword(!showPassword)}
                 className="password-toggle"
               >
-                {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+                {!showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
               </button>
             </div>
           </div>
